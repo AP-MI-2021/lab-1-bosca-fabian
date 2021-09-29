c04f9bf68e5ec269e2d86ab3c809d3869800e039
Bosca Fabian <fabian.bosca@stud.ubbcluj.ro>
2021-09-29 12:53:36 +0300

    Primul laborator rezolvat

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ Returneaza true daca n este prim si false daca nu.
 def is_prime(n):
   if n < 2:
     return False
-  for i in range(2, n // 2)
+  for i in range(2,n//2+1):
     if n % i == 0:
       return False
   return True
@@ -15,7 +15,7 @@ Returneaza produsul numerelor din lista lst.
 '''
 def get_product(lst):
   produs = 1
-  for i in lst
+  for i in lst:
     produs *= i
   return produs
   
@@ -24,7 +24,7 @@ def get_product(lst):
 Returneaza CMMDC a doua numere x si y folosind primul algoritm.
 '''
 def get_cmmdc_v1(x, y):
-  while x != y
+  while x != y:
     if x > y:
       x -= y
     else:
@@ -38,7 +38,7 @@ Returneaza CMMDC a doua numere x si y folosind al doilea algoritm.
 def get_cmmdc_v2(x, y):
   if y > x:
     x, y = y, x
-  while y != 0
+  while y != 0:
     rest = x % y
     x = y
     y = rest
@@ -46,7 +46,19 @@ def get_cmmdc_v2(x, y):
   
   
 def main():
+  n = int(input("Dati posibilul numar prim:"))
+  print(is_prime(n))
+  list = []
+  number_of_elements = int(input("Introduceti numarul de elemente ale listei:"))
+  for i in range(0,number_of_elements):
+    number = int(input("Introduceti element:"))
 
+    list.append(number)
+  print(get_product(list))
+  x = int(input("Primul numar pentru CMMDC:"))
+  y = int(input("Al doilea numar pentru CMMDC:"))
+  print("CMMDC_v1:",get_cmmdc_v1(x, y))
+  print("CMMDC_v2:",get_cmmdc_v2(x, y))
 
 if __name__ == '__main__':
   main()
