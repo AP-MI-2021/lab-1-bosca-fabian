a8b84dae779cc50f5f8d43c9c9b6c94913092510
Bosca Fabian <fabian.bosca@stud.ubbcluj.ro>
2021-09-29 12:09:18 +0300

    primul commit

--- main.py
+++ main.py
@@ -2,32 +2,51 @@
 Returneaza true daca n este prim si false daca nu.
 '''
 def is_prime(n):
-  # codul vostru aici
+  if n < 2:
+    return False
+  for i in range(2, n // 2)
+    if n % i == 0:
+      return False
+  return True
   
   
 '''
 Returneaza produsul numerelor din lista lst.
 '''
 def get_product(lst):
-  # codul vostru aici
+  produs = 1
+  for i in lst
+    produs *= i
+  return produs
   
   
 '''
 Returneaza CMMDC a doua numere x si y folosind primul algoritm.
 '''
 def get_cmmdc_v1(x, y):
-  # codul vostru aici
+  while x != y
+    if x > y:
+      x -= y
+    else:
+      y -= x
+  return x
   
   
 '''
 Returneaza CMMDC a doua numere x si y folosind al doilea algoritm.
 '''
 def get_cmmdc_v2(x, y):
-  # codul vostru aici
+  if y > x:
+    x, y = y, x
+  while y != 0
+    rest = x % y
+    x = y
+    y = rest
+  return x
   
   
 def main():
-  # interfata de tip consola aici
+
 
 if __name__ == '__main__':
   main()
